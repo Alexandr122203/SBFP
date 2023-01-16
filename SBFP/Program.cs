ae327a346ff3caedc96f355d12ea298aceefc975
sobak <sobak@LAPTOP-UQ9GHUT7>
2023-01-17 01:26:29 +1000

    Добавьте файлы проекта.

---
+++ SBFP/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Метод Write выводит текст без переноса строки:");
+Console.ReadKey();
+Console.Write ("Привет");
+Console.ReadKey ();
+Console.Write("мир");
+Console.ReadKey();
+Console.Write("!!!");
+Console.ReadKey();
+Console.WriteLine("");
+Console.ReadKey();
+Console.WriteLine("Метод WriteLine выводит текст с переносом строки после ввода:");
+Console.ReadKey();
+Console.WriteLine("Привет");
+Console.ReadKey();
+Console.WriteLine("мир");
+Console.ReadKey();
+Console.WriteLine("!!!");
+Console.ReadKey();
